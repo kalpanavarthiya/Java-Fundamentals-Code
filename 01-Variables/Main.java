aeba2c01e10e3752222a995523bc535d79f2b645
kalpanavarthiya <your-email@example.com>
2026-02-25 20:10:33 +0530

    feat: complete basic variables and data types practice

---
+++ 01-Variables/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 21;
+        String name = "Software Engineer 2026";
+        boolean isLearning = true;
+
+        System.out.println("Hello, my name is: " + name);
+        System.out.println("I am " + age + " years old.");
+        System.out.println("Am I learning Java? " + isLearning);
+    }
+}
